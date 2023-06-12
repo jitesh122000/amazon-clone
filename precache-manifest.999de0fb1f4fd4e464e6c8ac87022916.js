4d5bf1eec59c18fa93208bfb997dd41e62e1ad96
Jitesh <rakesh@Jitesh-MacBook-Pro.local>
2023-06-12 15:13:12 +0530

    Updates

---
+++ precache-manifest.999de0fb1f4fd4e464e6c8ac87022916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4da95eea2b98e09621a",
+    "url": "/amazone-clone/static/css/main.5eab58fd.chunk.css"
+  },
+  {
+    "revision": "d4da95eea2b98e09621a",
+    "url": "/amazone-clone/static/js/main.d4da95ee.chunk.js"
+  },
+  {
+    "revision": "676c8d3e0fc45ad1eec8",
+    "url": "/amazone-clone/static/js/1.676c8d3e.chunk.js"
+  },
+  {
+    "revision": "53b2ef58e760c8a23552",
+    "url": "/amazone-clone/static/js/2.53b2ef58.chunk.js"
+  },
+  {
+    "revision": "b5ba5cedfab111cd5a7f",
+    "url": "/amazone-clone/static/js/runtime~main.b5ba5ced.js"
+  },
+  {
+    "revision": "b9a78743a4b6465bac37c0cb901e3197",
+    "url": "/amazone-clone/index.html"
+  }
+];
